382441edfc4415097a0a3681a37f1ce850fbe5a2
BVK Chaitanya <bvk@steam.zion.sh>
2021-09-06 17:26:15 -0700

    Renamed Rollback method to Discard so that it works with snapshots.

--- tx.go
+++ tx.go
@@ -5,16 +5,17 @@ import "context"
 // Transaction lists the methods for read/write transactions.
 type Transaction interface {
 	Reader
+	Scanner
+
 	Writer
 	Deleter
-	Scanner
 
 	// Commit atomically makes all transaction changes durable and visible on the
 	// backing kv store.  Returns sql.ErrTxDone if transaction was already
 	// committed or rolled back.
 	Commit(ctx context.Context) error
 
-	// Rollback drops all changes performed by the transaction.  Returns
-	// sql.ErrTxDone if transaction was already committed or rolled back.
-	Rollback(ctx context.Context) error
+	// Discard drops all changes performed by the transaction.  Returns
+	// sql.ErrTxDone if transaction was already committed or discarded.
+	Discard(ctx context.Context) error
 }
